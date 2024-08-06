c27dc006da9cf12fed6ea13cfb2717e24b385c93
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-08-06 14:15:08 -0400

    JavaScript en práctica...

---
+++ ArchivosEjecutables/script.js
@@ -0,0 +1,23 @@
+// CÓMO AGREGAR JAVASCRIPT A NUESTRA HOJA EN FORMATO HTML...
+
+alert("NO TE DETENGAS, QUE ES MOMENTO DE ACTUAR!!!!!");
+
+/* ¿Qué son las APIs?
+
+    Los Interfaces de Programación de Aplicaciones (APIs por sus siglas en inglés) son construcciones
+    disponibles en los lenguajes de programación que permiten a los desarrolladores crear funcionalidades
+    complejas de una manera simple. Estas abstraen el código más complejo para proveer una sintaxis más
+    fácil de usar en su lugar.
+        
+    Como ejemplo, piensa en el suministro de electricidad de tu casa, apartamento o cualquier otro edificio. Si
+    quieres usar un electrodoméstico, simplemente lo conectas en un enchufe y funciona. No intentas
+    conectarlo directamente a la fuente de alimentación - hacerlo sería muy ineficiente y, si no eres 
+    electricista, difícil y peligroso.
+
+    De la misma manera, si quisieras programar gráficos 3D, sería mucho más facil 
+    hacerlo usando una API escrita en un lenguaje de alto nivel como JavaScript o 
+    Python, en lugar de intentar escribir código de bajo nivel (por ejemplo: C o C++) 
+    que controle directamente la GPU del equipo u otras funciones gráficas.
+
+*/
+
